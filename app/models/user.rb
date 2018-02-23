a2862d136db477f5d6018886a3d0cedd955b11c1
David Towster <djt4xe@virginia.edu>
2018-02-22 19:28:21 -0800

    Added user validation method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :garages
     has_many :cars, through: :garages
+
+    def self.valid_params?(params)
+        !(params[:username].empty? || params[:password].empty?)
+    end
 end
