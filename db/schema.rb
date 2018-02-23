b7bb92d0c91f06b5cbf3ef5939271e2c52f79ba4
David Towster <djt4xe@virginia.edu>
2018-02-22 18:43:22 -0800

    Ran db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string  "model"
+    t.string  "manufacturer"
+    t.string  "year"
+    t.integer "garage_id"
+  end
+
+  create_table "garages", force: :cascade do |t|
+    t.string  "name"
+    t.string  "capacity"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
