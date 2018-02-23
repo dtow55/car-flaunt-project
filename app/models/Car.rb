8be2725c45b7da61ae06a3d0dd26abe5d8ca383c
David Towster <djt4xe@virginia.edu>
2018-02-22 19:38:10 -0800

    Created Car.rb model file

---
+++ app/models/Car.rb
@@ -0,0 +1,7 @@
+class Car < ActiveRecord::Base
+    belongs_to :garage
+
+    def self.valid_params?(params)
+        !(params[:model].empty? || params[:manufacturer].empty? || params[:year].empty? || params[:garage_id].empty?)
+    end
+end
